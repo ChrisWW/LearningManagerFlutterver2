ea270dbe26463827db678dd064412a20bc31b33a
ChrisWW <krystian.wiewior@gmail.com>
2022-05-12 20:34:33 +0200

    initial commit

---
+++ lib/ui/screens/skeleton_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_production_boilerplate/cubit/bottom_nav_cubit.dart';
+import 'package:flutter_production_boilerplate/ui/screens/first_screen.dart';
+import 'package:flutter_production_boilerplate/ui/screens/second_screen.dart';
+import 'package:flutter_production_boilerplate/ui/widgets/app_bar_gone.dart';
+import 'package:flutter_production_boilerplate/ui/widgets/bottom_nav_bar.dart';
+
+class SkeletonScreen extends StatelessWidget {
+  const SkeletonScreen({Key? key}) : super(key: key);
+
+  final _pageNavigation = const [
+    FirstScreen(),
+    SecondScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<BottomNavCubit>(
+        create: (context) => BottomNavCubit(),
+        child: Scaffold(
+          appBar: const AppBarGone(),
+
+          /// When switching between tabs this will fade the old
+          /// layout out and the new layout in.
+          body: BlocBuilder<BottomNavCubit, int>(
+            builder: (context, state) {
+              return AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  child: _pageNavigation.elementAt(state));
+            },
+          ),
+
+          /// Cannot be const, tab status will not update.
+          bottomNavigationBar: BottomNavBar(),
+        ));
+  }
+}
