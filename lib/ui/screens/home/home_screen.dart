d2a9d9097b483e0b4518b52f3369f0dffc8619e8
ChrisWW <krystian.wiewior@gmail.com>
2022-07-04 09:36:46 +0200

    page drawe

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -32,7 +32,7 @@ class _HomeScreenState extends State<HomeScreen> {
         return DefaultTabController(
           length: 3,
           child: Scaffold(
-            // drawer: const DrawerPage(),
+            drawer: const DrawerPage(),
             appBar: AppBar(
                 foregroundColor: Colors.black,
                 title: Text("Learning Manager"),
